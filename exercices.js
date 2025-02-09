8afb5789cef6a74e454b1eb5ac94958bfb157cc7
Edo57 <99027738+Edo57@users.noreply.github.com>
2025-02-09 12:35:37 +0100

    Update exercices.js

--- exercices.js
+++ exercices.js
@@ -2,7 +2,6 @@ const myboutique = "HellTraps";
 const mypotions = 55;
 const pricepotions = 10;
 const OpenorNot = true;
-
 if (OpenorNot === true) {
   console.log(
     "Welcome to " +
@@ -15,4 +14,23 @@ if (OpenorNot === true) {
   );
 } else {
   console.log("Sorry, we are closed today.");
+}
+const choice = prompt(
+  "What can we offer you today? 1. Name of our shop 2. Name of the wizard 3. Price of the potions 4. Number of potions?"
+);
+switch (choice) {
+  case "1":
+    console.log("Our shop is called " + myboutique + ".");
+    break;
+  case "2":
+    console.log("Our wizard is called Merlin.");
+    break;
+  case "3":
+    console.log("Our potions are sold at " + pricepotions + " gold each.");
+    break;
+  case "4":
+    console.log("We have " + mypotions + " potions in stock.");
+    break;
+  default:
+    console.log("Invalid choice.");
 }
