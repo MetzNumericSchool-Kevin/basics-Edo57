a5fbd8e74b0bcdedbb1e41ce66d77ae283db79ea
Edo57 <99027738+Edo57@users.noreply.github.com>
2025-02-09 12:11:41 +0100

    Update exercices.js

--- exercices.js
+++ exercices.js
@@ -2,3 +2,17 @@ const myboutique = "HellTraps";
 const mypotions = 55;
 const pricepotions = 10;
 const OpenorNot = true;
+
+if (OpenorNot === true) {
+  console.log(
+    "Welcome to " +
+      myboutique +
+      " ! We have " +
+      mypotions +
+      " potions at " +
+      pricepotions +
+      " gold each. We are open to serve you."
+  );
+} else {
+  console.log("Sorry, we are closed today.");
+}
